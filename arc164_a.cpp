93f2df4de2e5db6661a10652b4533ed367bd7c91
spaceybread <Spaceybread@gmail.com>
2023-08-28 18:42:33 +0530

    Create arc164_a.cpp

---
+++ arc164_a.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <math.h>
+#include <string>
+#include <cmath>
+#ifndef _GLIBCXX_NO_ASSERT
+#include <cassert>
+#endif
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+#if __cplusplus >= 201103L
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#endif
+
+// C++
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+
+using namespace std;
+
+int main() {
+        
+    long long int t;
+    
+    cin >> t;
+    
+    for (int j = 0; j < t; j++) {
+        
+        long long int n, k;
+        
+        cin >> n >> k;
+        
+        long long int i = 0;
+        
+        while (n > 0) {
+            
+            i = i + n % 3;
+
+            n = n - n % 3;
+            
+            n = n / 3;
+
+        }
+        
+        if ((i % 2 == k % 2) && i <= k) {
+            cout << "Yes" << "\n";
+        } else {
+            cout << "No" << "\n";
+        }
+    }
+}
