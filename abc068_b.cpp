6784ad7f3ae3d6392b8503c746c0e0d11cefe27b
spaceybread <Spaceybread@gmail.com>
2023-09-05 18:50:38 -0500

    Create abc068_b.cpp

---
+++ abc068_b.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std; 
+
+int main() {
+  int n;
+  cin >> n;
+  int i = 1;
+  
+  while (i <= n) {
+    i = i * 2; 
+  }
+  
+  cout << i/2 << "\n";
+  
+  
+}
