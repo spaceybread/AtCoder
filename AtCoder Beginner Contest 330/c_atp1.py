25ee91d40e4ca8ae8bc6030010d37546729a6e9d
spaceybread <Spaceybread@gmail.com>
2023-11-25 15:55:57 -0600

    Create c_atp1.py

---
+++ AtCoder Beginner Contest 330/c_atp1.py
@@ -0,0 +1,27 @@
+#TLE
+
+import math
+
+D = int(input())
+
+d = math.floor(math.sqrt(D))
+min = D
+i = 0
+
+while i**2 <= D:
+    
+    c = math.floor(math.sqrt(D - i**2))
+    #print(i**2)
+    #print(c)
+    if c % 1 == 0:
+        C = D - i**2 - c**2
+        if C < min:
+            #print(i**2)
+            min = C
+            #if min == 0:
+             #   print(i)
+    
+    i = i + 1
+
+print(int(min))
+
