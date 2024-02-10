005a8bbedf1aed121dc53506231ab0731995b48b
spaceybread <Spaceybread@gmail.com>
2024-02-10 08:04:46 -0600

    Update c_at2.py

--- AtCoder Beginner Contest 340/c_at2.py
+++ AtCoder Beginner Contest 340/c_at2.py
@@ -1,5 +1,7 @@
 # i thought i had a creative solution, turns out it was not a solution
 
+# as per this comment: https://www.youtube.com/watch?v=jMu_1zzJESA&lc=Ugzn0D5L8sSTVYaeKpV4AaABAg.A-dKD8UkzULA-dMIpUNJco, it's because floating point numbers weren't sufficient for this constraint 
+
 import math
 n = int(input())
 val = math.floor(math.log2(n))
