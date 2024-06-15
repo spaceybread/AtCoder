4af531244f59508eced593cb156a67d9349cdddf
spaceybread <Spaceybread@gmail.com>
2024-06-15 08:46:10 -0500

    Create a.py

---
+++ AtCoder Beginner Contest 358/a.py
@@ -0,0 +1,11 @@
+#n = int(input())
+#n, m = (map(int, input().split()))
+#a = list(map(int, input().split()))
+#b = list(map(int, input().split()))
+a = input().split()
+
+if a[0] == "AtCoder" and a[1] == "Land":
+    print("Yes")
+else:
+    print("No")
+
