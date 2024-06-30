b73e4c00f760f1056e416c4bda87689d7628e906
spaceybread <Spaceybread@gmail.com>
2024-06-30 10:31:12 -0500

    Create a.py

---
+++ AtCoder Beginner Contest 360/a.py
@@ -0,0 +1,12 @@
+#n = int(input())
+#n, m = (map(int, input().split()))
+#a = list(map(int, input().split()))
+#b = list(map(int, input().split()))
+
+s = input()
+
+if s.index('R') < s.index('M'):
+    print('Yes')
+else:
+    print('No')
+
