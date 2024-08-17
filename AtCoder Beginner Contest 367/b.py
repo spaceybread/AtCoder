f114df0ff452d6dd5c887ecd2972312392c6d035
spaceybread <Spaceybread@gmail.com>
2024-08-17 19:29:02 +0530

    Create b.py

---
+++ AtCoder Beginner Contest 367/b.py
@@ -0,0 +1,15 @@
+#n = int(input())
+#a, b, c = (map(int, input().split()))
+#a = list(map(int, input().split()))
+#b = list(map(int, input().split()))
+
+x = input()
+
+f = float(x)
+n = int(f)
+
+if n == f:
+    print(n)
+else:
+    print(f)
+
